e4b4accc35a4f5ee533922f3311058cdc81c9a9c
kas <code@kasthe.dev>
2026-09-17 23:18:23 -0500

    build(web): refresh embedded home assets

--- apps/api/internal/webassets/dist/_app/immutable/nodes/6.ypQt87Ft.js
+++
@@ -1,1 +0,0 @@
-import{Ot as e,kt as t}from"../chunks/DaPaBlc-.js";import"../chunks/xihTtKlq.js";import{t as n}from"../chunks/C44OKVIJ.js";function r(r,i){t(i,!0),n(r,{get routeWorkspaceID(){return i.params.workspaceID}}),e()}export{r as component};
