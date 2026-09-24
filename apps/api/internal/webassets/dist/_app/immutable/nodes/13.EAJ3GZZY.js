17085adf8dac6d9e924f170968a5f915ef80f8d3
kas <code@kasthe.dev>
2026-09-24 18:00:22 -0500

    embed spinner correlation fix in web assets

---
+++ apps/api/internal/webassets/dist/_app/immutable/nodes/13.EAJ3GZZY.js
@@ -0,0 +1,1 @@
+import{Mt as e,jt as t}from"../chunks/BkY25Hs-.js";import"../chunks/xihTtKlq.js";import{t as n}from"../chunks/CLqAgtx5.js";function r(r,i){e(i,!0),n(r,{get routeWorkspaceID(){return i.params.workspaceID},get routeTargetID(){return i.params.targetID}}),t()}export{r as component};
